86012786e3c12d0074784fe039188726f67d8ea3
nitzansaar <112202258+nitzansaar@users.noreply.github.com>
2024-10-25 14:42:45 -0700

    MineFieldTester

--- MineFieldTester.java
+++ MineFieldTester.java
@@ -1,8 +1,34 @@
 public class MineFieldTester {
-    public static void main(String[] args) {
-        boolean[][] mine = new boolean[10][10];
-        MineField m = new MineField(mine);
-        System.out.println(m.toString());
 
+    public static MineField mineField;
+    public static void main(String[] args) {
+        // MineField mineField = new MineField(10, 10, 3);
+        // System.out.println(mineField.toString());
+        // System.out.println(mineField.numMines());
+        // mineField.populateMineField(1, 1);
+        // System.out.println(mineField.toString());
+        testPopulateMineField();
+    }
+
+    private static void testPopulateMineField() {
+        mineField = new MineField(100, 100, 43);
+        mineField.populateMineField(1, 1);
+        System.out.println(mineField.toString());
+        System.out.println(countMines(mineField));
+        // assert countMines(mineField);
+        
+    }
+    private static boolean countMines(MineField mineField) {
+        int rows = mineField.numRows();
+        int cols = mineField.numCols();
+        int mineCount = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mineField.hasMine(i, j)) {
+                    mineCount++;
+                }
+            }
+        }
+        return mineCount == mineField.numMines();
     }
 }
